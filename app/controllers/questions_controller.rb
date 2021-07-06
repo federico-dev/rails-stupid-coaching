d17d6fc0b26982ec61178107cb3091c56822bb03
Federico Lorenzon <lorenzon.federico@gmail.com>
2021-07-06 20:54:08 +0200

    adding challengeù

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,17 @@
 class QuestionsController < ApplicationController
   def ask
   end
+  
+  def answer
+    @question = params[:question]
+    
+    if @question.downcase == "i am going to work right now!"
+        puts ""
+      elsif @question.end_with?("?")
+        puts "Silly question, get dressed and go to work!"
+      else
+        puts "I don't care, get dressed and go to work!"
+      end
+    end
+    helper_method :answer
 end
